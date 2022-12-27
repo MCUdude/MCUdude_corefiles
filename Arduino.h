a9ade41df4b5d8a5dc1e051c4aeecbfc62e046d2
MCUdude <hansibull@gmail.com>
2022-12-27 10:03:29 +0100

    Use template functions instead of macros by default This will make it possible to use the previous "typeof" macros outside functions and structures

--- Arduino.h
+++ Arduino.h
@@ -143,16 +143,9 @@ void yield(void);
 #ifdef abs
 #undef abs
 #endif
-#define abs(x)       ({ typeof (x) _x = (x); _x > 0 ? _x : -_x; })
-#define sq(x)        ({ typeof (x) _x = (x); _x * _x; })
-#define round(x)     ({ typeof (x) _x = (x); _x >= 0 ? (long)(_x + 0.5) : (long)(_x - 0.5); })
+
 #define radians(deg) ((deg) * DEG_TO_RAD)
 #define degrees(rad) ((rad) * RAD_TO_DEG)
-#define constrain(x,low,high)     ({ \
-  typeof (x) _x = (x);               \
-  typeof (low) _l = (low);           \
-  typeof (high) _h = (high);         \
-  _x < _l ? _l : _x > _h ? _h : _x; })
 
 #define interrupts() sei()
 #define noInterrupts() cli()
@@ -314,17 +307,46 @@ extern const uint8_t PROGMEM digital_pin_to_timer_PGM[];
 #endif
 
 #ifdef __cplusplus
+	template<class T>
+	auto abs(const T& x) -> decltype(x > 0 ? x : -x) {
+		return x > 0 ? x : -x;
+	}
+
 	template<class T, class L>
 	auto min(const T& a, const L& b) -> decltype((b < a) ? b : a) {
 		return (b < a) ? b : a;
 	}
+
 	template<class T, class L>
 	auto max(const T& a, const L& b) -> decltype((b < a) ? b : a) {
 		return (a < b) ? b : a;
 	}
+
+	template<class T>
+	long round(const T& x) {
+		return (long)(x >= 0 ? (x + 0.5) : (x - 0.5));
+	}
+
+	template<class T>
+	auto sq(const T& x) -> decltype(x * x) {
+		return x * x;
+	}
+
+  template<class T, class L, class H>
+	auto constrain(const T& x, const L& l, const H& h) -> decltype((x < l) ? l : (x > h) ? h : x) {
+		return (x < l) ? l : (x > h) ? h : x;
+	}
+
 #else
-  #define min(a,b)   ({ typeof (a) _a = (a); typeof (b) _b = (b); _a < _b ? _a : _b; })
-  #define max(a,b)   ({ typeof (a) _a = (a); typeof (b) _b = (b); _a > _b ? _a : _b; })
+  #define abs(x)       ({ typeof (x) _x = (x); _x > 0 ? _x : -_x; })
+  #define min(a,b)     ({ typeof (a) _a = (a); typeof (b) _b = (b); _a < _b ? _a : _b; })
+  #define max(a,b)     ({ typeof (a) _a = (a); typeof (b) _b = (b); _a > _b ? _a : _b; })
+  #define sq(x)        ({ typeof (x) _x = (x); _x * _x; })
+  #define constrain(x,low,high)     ({ \
+  typeof (x) _x = (x);               \
+  typeof (low) _l = (low);           \
+  typeof (high) _h = (high);         \
+  _x < _l ? _l : _x > _h ? _h : _x; })
 #endif // __cplusplus
 
 #ifdef __cplusplus
